a88f96f3d7917eda9e473ec9ccc093a9b98fed1d
MichaelScutts100060445 <158296712+MichaelScutts100060445@users.noreply.github.com>
2024-02-17 18:00:07 +0000

    Created methods to create a surface for the tiles and fill the tiles with the color declared when making a tile object, the surface and fill are done automatically when creating an object

--- Board/tiles.py
+++ Board/tiles.py
@@ -3,5 +3,14 @@ class Tiles:
     def _init_(self, color):
         self.width, self.height =  100
         self.color = color
+        self.surface = self.CreateTileSurface()
+        
+        self.FillSurface()
+        
+    def CreateTileSurface(self):
+        return pygame.Surface((self.width, self.height))
+    
+    def FillSurface(self):
+        self.surface.fill(self.color)
         
     
