c0ef285c63994ec3ac1914fec433aead803ec3f6
Raikiri45 <149266085+Raikiri45@users.noreply.github.com>
2024-02-17 23:08:28 +0000

    Created board components folder. Inside is an init file that allows packages/ classes to be imported into files outside the folder. Tiles and board class created and some fields and classes initalised

---
+++ boardComponents/tiles.py
@@ -0,0 +1,18 @@
+import pygame
+from board import Board
+class Tiles:
+    # A tile object takes color, usually an rgb tuple, and position which is a tuple containing a char and an int
+    # Which should come from board
+    def __init__(self,color, position):
+        self.width, self.height =  100, 100
+        self.color = color
+        self.surface = self.CreateTileSurface()
+        self.xPosition, self.yPosition = position
+        
+        self.FillSurface()
+        
+    def CreateTileSurface(self):
+        return pygame.Surface((self.width, self.height))
+    
+    def FillSurface(self):
+        self.surface.fill(self.color)
