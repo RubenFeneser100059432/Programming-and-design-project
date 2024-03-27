514e1d5fa2c4e2dd709504ed6e791e59150e0276
RubenFeneser100059432 <158295286+RubenFeneser100059432@users.noreply.github.com>
2024-03-27 13:26:23 +0000

    FINAL so far completed
    The final one so far, however not finished yet

--- Mainmenu3.py
+++ Mainmenu3.py
@@ -22,7 +22,7 @@ def main_menu():
 
     while True: # started our loop
         playButtonRect = pygame.Rect(screenWidth // 2 - 100, screenHeight // 2 - 50, 200, 100) # start button variable position
-
+        quitButtonRect = pygame.Rect(screenWidth // 2 - 100, screenHeight // 2 - -100, 200, 100) # quit button variable positioning
         # Iterates over every action taken by the user
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # if the event type is quitting pygame application, exit pygame and sys.
@@ -33,15 +33,20 @@ def main_menu():
                     # if the event taken was a mouse click by the user, and the collidepoint is on the start button;
                     # Transition to next screen
                     secondScreen()
+                if quitButtonRect.collidepoint(pygame.mouse.get_pos()):
+                    pygame.quit()
+                    sys.exit()
                     
 
         screen.blit(BG, (0, 0)) # Here we initialise the BG variable to set background of menu screen
         draw_text("Chess Game", pygame.font.Font(None, 60), (0, 0, 0), screen, screenWidth // 2, 100) # title drawn using draw_text function
         colourOfButton = (0, 0, 0) # addding colour of start button
+        colourOfQuitButton = (20,20,20) # colour of quit button
         pygame.draw.rect(screen, colourOfButton, playButtonRect) # drawing start button
+        pygame.draw.rect(screen, colourOfQuitButton, quitButtonRect) # drawing quit button
         # taken from existing pygame Font class
         draw_text("Start", pygame.font.Font(None, 60), (255, 0, 0), screen, screenWidth // 2, screenHeight // 2,)
-
+        draw_text("Quit", pygame.font.Font(None, 60), (255, 0, 0), screen, screenWidth // 2, screenHeight // 1.4)
         pygame.display.flip() # turns to next screen once button clicked
         clock.tick(60)
 
@@ -49,6 +54,7 @@ def secondScreen(): # second screen(colour choice) function
     while True: # same loop as first screen
         whiteButtonRect = pygame.Rect(screenWidth // 2 - -100, screenHeight // 2 - 50, 200, 100) # white button variable
         blackButtonRect = pygame.Rect(screenWidth // 2 - 350, screenHeight // 2 - 50, 200, 100) # black button variable
+        backButtonRect = pygame.Rect(screenWidth // 2 - 100, screenHeight // 2- -100, 200, 100) # back button variable
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -59,15 +65,20 @@ def secondScreen(): # second screen(colour choice) function
                         main_menu()
                     if blackButtonRect.collidepoint(pygame.mouse.get_pos()):
                         main_menu()
+                    if backButtonRect.collidepoint(pygame.mouse.get_pos()):
+                        main_menu() # this one does stay like this... 
                         
         screen.blit(BG, (0, 0))
         draw_text("Player One, Please Choose Your Colour", pygame.font.Font(None, 60), (0, 0, 0), screen, screenWidth // 2, 100)
         colourOfButtonWhite = (255,255,255) # adding colour of white button
         colourOfButtonBlack = (0,0,0) # adding colour of black button
+        colourOfButtonBack = (20,20,20) # colour of back button
         pygame.draw.rect(screen, colourOfButtonWhite, whiteButtonRect) # drawing white button on screen
         pygame.draw.rect(screen, colourOfButtonBlack, blackButtonRect) # drawing black button on screen
+        pygame.draw.rect(screen, colourOfButtonBack, backButtonRect) # drawing back button on screen
         draw_text("White", pygame.font.Font(None, 60), (0,0,0), screen, screenWidth // 1.4, screenHeight // 2,)
         draw_text("Black", pygame.font.Font(None, 60), (255,255,255), screen, screenWidth // 4, screenHeight // 2,)
+        draw_text("Back", pygame.font.Font(None, 60), (255,0,0), screen, screenWidth // 2, screenHeight // 1.4) #same as quit button positioning
         
         pygame.display.flip() # in this instance flips back to main menu screen (for now...)
         clock.tick(60)
